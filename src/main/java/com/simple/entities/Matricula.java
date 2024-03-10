d6dae1cdbda3e8cfad08729c6ec070035508b012
fsilvaso <fsilvaso@indracompany.com>
2024-03-10 18:13:50 -0300

    add configuracao do banco de dados

--- src/main/java/com/simple/entities/Matricula.java
+++ src/main/java/com/simple/entities/Matricula.java
@@ -4,6 +4,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinColumns;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -17,7 +20,13 @@ public class Matricula {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private Long idCurso;
-    private Long edEstudante;
+
+    @ManyToOne
+    @JoinColumn(name = "curso_id")
+    private Curso curso;
+
+    @ManyToOne
+    @JoinColumn(name = "estudante_id")
+    private Estudante estudante;
 
 }
