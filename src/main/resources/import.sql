e62328ed5f323a468900324f894aff3290c327cd
Fabio Soares <fabao1966@yahoo.com.br>
2024-03-11 20:39:14 -0300

    modified application.properties, import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,15 +1,33 @@
-INSERT INTO cursos(creditos, titulo) VALUES (6, 'Construção de API RESTful utilizando Spring Boot');
-INSERT INTO cursos(creditos, titulo) VALUES (8, 'Gestão');
-INSERT INTO cursos(creditos, titulo) VALUES (20, 'SISTEMAS DE INFORMAÇÃO');
+INSERT INTO
+    cursos (creditos, titulo)
+VALUES (
+        6, 'Construção de API RESTful utilizando Spring Boot'
+    );
 
+INSERT INTO cursos (creditos, titulo) VALUES (8, 'Gestão');
 
+INSERT INTO
+    cursos (creditos, titulo)
+VALUES (20, 'SISTEMAS DE INFORMAÇÃO');
 
-INSERT INTO estudantes(data_matricula, nome) VALUES ('1582-10-15', 'Penelope Charmosa');
-INSERT INTO estudantes(data_matricula, nome) VALUES ('1582-10-15', 'Diana Prince');
+INSERT INTO
+    estudantes (data_matricula, nome)
+VALUES (
+        '1582-10-15', 'Penelope Charmosa'
+    );
 
-INSERT INTO estudantes(data_matricula, nome) VALUES ('2024-02-29', 'Manda chuva');
+INSERT INTO
+    estudantes (data_matricula, nome)
+VALUES ('1582-10-15', 'Diana Prince');
 
-INSERT INTO estudantes(data_matricula, nome) VALUES ('2024-02-29', 'Zé Colmeia');
+INSERT INTO
+    estudantes (data_matricula, nome)
+VALUES ('2024-02-29', 'Manda chuva');
 
-INSERT INTO matriculas(curso_id, estudante_id) VALUES (1, 2);
-INSERT INTO matriculas(curso_id, estudante_id) VALUES (2, 1);
+INSERT INTO
+    estudantes (data_matricula, nome)
+VALUES ('2024-02-29', 'Ze Colmeia');
+
+INSERT INTO matriculas (curso_id, estudante_id) VALUES (1, 2);
+
+INSERT INTO matriculas (curso_id, estudante_id) VALUES (2, 1);
