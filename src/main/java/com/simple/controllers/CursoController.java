24a7348ef9911d782b01fd5d52e17973a4c946fe
fsilvaso <fsilvaso@indracompany.com>
2024-03-11 08:54:27 -0300

    added update and delete function

--- src/main/java/com/simple/controllers/CursoController.java
+++ src/main/java/com/simple/controllers/CursoController.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -35,7 +37,7 @@ public class CursoController {
 
     
     @GetMapping("/{id}")
-    public Curso findById(@PathVariable @Validated Long id){
+    public Curso cursoFindById(@PathVariable @Validated Long id){
         log.info("Get cursos by id", id);
 
         var  result =  repository.findById(id).get();
@@ -51,6 +53,27 @@ public class CursoController {
             Curso  result =  repository.save(curso);
     
             return result;
+    }
+
+    
+    @PutMapping
+    public Curso update(@RequestBody @Validated Curso curso){
+            log.info("update course", curso);
+
+            Curso  result =  repository.save(curso);
+    
+            return result;
+    }
+
+     
+    @DeleteMapping
+    public void deleteCurso(@Validated @PathVariable Long id){
+        log.info("delete course by id", id);
+
+        Curso  result =  cursoFindById(id);
+
+        repository.delete(result);
+    
     }
     
 }
