9c306a9f4b02f77624665b172abc9a813e38a333
fsilvaso <fsilvaso@indracompany.com>
2024-03-11 16:59:44 -0300

    added delete endpoint

--- src/main/java/com/simple/controllers/MatriculaController.java
+++ src/main/java/com/simple/controllers/MatriculaController.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -36,19 +38,30 @@ public class MatriculaController {
     @GetMapping("/{id}")
     public Matricula matriculaById(@PathVariable @Validated Long id){
         log.info("Get registration by id", id);
-
         var  result =  repository.findById(id).get();
-
         return result;
     }
 
     @PostMapping
     public Matricula insert(@RequestBody @Validated Matricula matricula){
-            log.info("Insert new registration", matricula);
+        log.info("Insert new registration", matricula);
+        Matricula  result =  repository.save(matricula);
+        return result;
+    }
 
-            Matricula  result =  repository.save(matricula);
     
-            return result;
+    @PutMapping
+    public Matricula update(@RequestBody @Validated Matricula matricula){
+        log.info("update registration", matricula);
+        Matricula  result =  repository.save(matricula);    
+        return result;
+    }
+
+    @DeleteMapping
+    public void delete(@Validated @PathVariable Long id){
+        log.info("delete registration by id", id);
+        Matricula  result =  matriculaById(id);
+        repository.delete(result);    
     }
     
 }
